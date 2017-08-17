dcc61790b2ebefa3efe03d8cd11263fac1d63bec
Bálint Hegyi <hegyi.balint.b@gmail.com>
2017-08-17 16:27:45 +0200

    Create array test

--- src/test/java/hu/bme/mit/cch/MyNeo4jTest.java
+++ src/test/java/hu/bme/mit/cch/MyNeo4jTest.java
@@ -7,18 +7,41 @@ import org.neo4j.graphdb.QueryStatistics;
 import org.neo4j.graphdb.Result;
 import org.neo4j.test.TestGraphDatabaseFactory;
 
+import java.net.URL;
 import java.util.Arrays;
 
 public class MyNeo4jTest {
 
+    static String getResourceAbsolutePath(String testResource) {
+        final URL resource = MyNeo4jTest.class.getClassLoader().getResource(testResource);
+        System.out.println(resource);
+        return resource.getPath();
+    }
+
     @Test
-    public void hello() {
+    public void basicTest() {
         final String header = ":ID|name:STRING";
 
         final CsvLoaderConfig config = CsvLoaderConfig.builder().fieldTerminator('|').stringIds(false).build();
         final CsvHeaderToCypherConverter c = new CsvHeaderToCypherConverter();
-        final String cypher = c.convertNodes("/home/szarnyasg/git/cypher-csv-header/src/test/resources/basic.csv", header, Arrays.asList("Person"), config);
 
+        final String cypher = c.convertNodes(getResourceAbsolutePath("basic.csv"), header, Arrays.asList("Person"), config);
+        System.out.println(cypher);
+
+        final GraphDatabaseService gds = new TestGraphDatabaseFactory().newImpermanentDatabase();
+        final Result execute = gds.execute(cypher);
+        final QueryStatistics qs = execute.getQueryStatistics();
+        Assert.assertEquals(1, qs.getNodesCreated());
+    }
+
+    @Test
+    public void arrayTest() {
+        final String header = ":ID|name:STRING[]";
+
+        final CsvLoaderConfig config = CsvLoaderConfig.builder().fieldTerminator('|').arrayDelimiter(':').stringIds(false).build();
+        final CsvHeaderToCypherConverter c = new CsvHeaderToCypherConverter();
+
+        final String cypher = c.convertNodes(getResourceAbsolutePath("array.csv"), header, Arrays.asList("Person"), config);
         System.out.println(cypher);
 
         final GraphDatabaseService gds = new TestGraphDatabaseFactory().newImpermanentDatabase();
