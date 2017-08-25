a4941a205c63db907c339c67110e67e3a3fab3e4
Gabor Szarnyas <szarnyasg@gmail.com>
2017-08-25 12:51:34 +0200

    Fix typo

--- src/test/java/hu/bme/mit/cch/TestConstants.java
+++ src/test/java/hu/bme/mit/cch/TestConstants.java
@@ -4,7 +4,7 @@ public class TestConstants {
 
 	public static final String TEST_NAME = "name";
     public static final String TEST_TYPE = "Type";
-    public static final String TEST_IDPSACE = "IDSPACE";
+    public static final String TEST_IDSPACE = "IDSPACE";
     public static final String TEST_ARRAY = "[]";
 
 	public static final String TEST_FIELD_1 = "name:Type(IDSPACE)[]";
